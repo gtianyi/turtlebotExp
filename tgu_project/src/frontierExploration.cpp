ea59331229bb174f466859f46fe96d2bbfaee6fd
guty <2008tygu@gmail.com>
2018-05-05 13:33:45 -0400

    froninter_map bugfree

--- tgu_project/src/frontierExploration.cpp
+++ tgu_project/src/frontierExploration.cpp
@@ -318,7 +318,7 @@ void FrontierExploration::connectedComponentLabeling() {
 
     for (int i = 0; i < GRID_SIZEY; i++) {
         for (int j = 0; j < GRID_SIZEX; j++) {
-            if (map2D[i][j].onFrontier) {
+            if (!map2D[i][j].onFrontier) {
                 continue;
             } else if (i - 1 >= 0 && j - 1 >= 0 &&
                     map2D[i - 1][j - 1].frontierID != -1) {
